d9704a60840a211130a4f813587aa7f6ef0bb9a3
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-01-07 15:57:21 +0530

    Add files via upload

---
+++ Basic_idle.py
@@ -0,0 +1,63 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: E:/Harsh_DataAnalytics/Basic_intro.py ============
+Hello
+Sum of x and y is: 79
+Hello
+Sum of 56 and 23 is: 79
+Sum of 56 and 23 is 79
+Div of 56 and 23 is 2
+Div of 56 and 23 is 2.4347826086956523
+Div of 56 and 23 is 2.4347826086956523
+
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+st = "This is python"
+'i' in st
+True
+a = 5
+b = 5
+a is b
+True
+li = [1,2,3]
+m = li
+m
+[1, 2, 3]
+li
+[1, 2, 3]
+m is li
+True
+n = li.copy()
+>>> n
+[1, 2, 3]
+>>> n is li
+False
+>>> import sys
+>>> sys.getsizeof(0)
+28
+>>> sys.getsizeof(0.0)
+24
+>>> sys.getsizeof("")
+41
+>>> sys.getsizeof("a")
+42
+>>> sys.getsizeof([])
+56
+>>> sys.getsizeof(())
+40
+>>> sys.getsizeof({})
+64
+>>> sys.getsizeof(set())
+216
+st = set()
+sys.getsizeof(st)
+216
