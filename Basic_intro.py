5cd29a6f2280b6cf439de658c7b802dd6dc63c4e
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-01-07 15:33:40 +0530

    Add files via upload

---
+++ Basic_intro.py
@@ -0,0 +1,23 @@
+#Print statements
+print("Hello")
+x = 56
+y = 23
+print("Sum of x and y is:",x+y,sep = " ",end = "\n")
+print('Hello')
+print("Sum of",x,"and",y,"is:",x+y)
+print("Sum of %d and %d is %d"%(x,y,x+y))
+print("Div of %d and %d is %d"%(x,y,x/y))
+print("Div of {} and {} is {}".format(x,y,x/y))
+print(f"Div of {x} and {y} is {x/y}"
+      )
+print()
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+""")
